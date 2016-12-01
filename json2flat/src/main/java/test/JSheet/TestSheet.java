53bcf5432079027d541f084ea8ead80318972fa7
opendevl <sanu.patel@hotmail.com>
2016-12-01 19:39:17 +0530

    updating TestSheet.java file

--- json2flat/src/main/java/test/JSheet/TestSheet.java
+++ json2flat/src/main/java/test/JSheet/TestSheet.java
@@ -23,14 +23,19 @@ public class TestSheet {
 		String str = new String(Files.readAllBytes(Paths.get("/home/workspace/mvgitproj/Json2Flat/json2flat/src/main/resources/test.json")));
 		JFlat flatMe = new JFlat(str);
 		
+		//get the 2D representation of JSON document
 		List<Object[]> json2csv = flatMe.json2Sheet().getJsonAsSheet();
 		
+		//write the 2D representation in csv format
 		flatMe.write2csv("/home/Desktop/json2csv.csv");
 		/*
 		 * OR
 		 * */
-		
+		//directly write the JSON document to CSV
 		flatMe.json2Sheet().write2csv("/home/Desktop/json2csv.csv");
+		
+		//directly write the JSON document to CSV but with delimiter
+		flatMe.json2Sheet().write2csv("/home/Desktop/json2csv.csv", '|');
 	}
 
 }
